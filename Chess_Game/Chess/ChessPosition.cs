a13014a815a39426c11ad7fc287bcb87ebd7135c
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-11-12 09:30:50 -0300

    @johnnymarcelino [ChessPosition class created] {nov.12.2022 at 9:30am}

---
+++ Chess_Game/Chess/ChessPosition.cs
@@ -0,0 +1,27 @@
+﻿using Game_Board;
+using System;
+
+namespace Chess
+{
+    internal class ChessPosition
+    {
+        public char BoardColumn { get; set; }
+        public int BoardLine { get; set; }
+
+        public ChessPosition(char boardColumn, int boardLine)
+        {
+            BoardColumn = boardColumn;
+            BoardLine = boardLine;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - BoardLine, BoardColumn - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + BoardColumn + BoardLine;
+        }
+    }
+}
--- Chess_Game/Program.cs
+++ Chess_Game/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Chess;
 using Game_Board;
 using System;
+using System.IO;
 
 namespace Chess_Game
 {
@@ -8,12 +9,21 @@ namespace Chess_Game
     {
         static void Main(string[] args)
         {
+
+            ChessPosition chessposition = new ChessPosition('c', 7);
+
+            Console.WriteLine(chessposition);
+
+            Console.WriteLine(chessposition.ToPosition());
+
+            /*
+
             try
             {
                 GameBoard gmbd = new GameBoard(8, 8);  // tipo matriz inicia-se com valor nulo.
 
-                gmbd.PutPiece(new Tower(gmbd, Color.Preta), new Position(0, 0));
-                gmbd.PutPiece(new Tower(gmbd, Color.Preta), new Position(8, 3));
+                gmbd.PutPiece(new Tower(gmbd, Color.Preta), new Position(1, 5));
+                gmbd.PutPiece(new Tower(gmbd, Color.Preta), new Position(4, 3));
                 gmbd.PutPiece(new Rey(gmbd, Color.Preta), new Position(1, 3));
                 Screen.GameBoardPrint(gmbd);
             }
@@ -22,6 +32,8 @@ namespace Chess_Game
                 Console.WriteLine(e.Message);
             }
             Console.ReadLine();
+
+            */
         }
     }
 }
